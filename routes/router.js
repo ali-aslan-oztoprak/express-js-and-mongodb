6bd1b8c75680f083a3154174fc158ecd1b3169a8
Ali Aslan Macbook <aliaslan.bilisim@gmail.com>
2019-09-29 23:38:56 +0300

    Controller folder name is changed

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controller/userController');
+const userController = require('../controllers/userController');
 
 router.get('/', (req, res, next) => {
     res.render('pages/index');
