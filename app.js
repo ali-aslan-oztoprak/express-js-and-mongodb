e0bcdee78fa7b41f4f31ab184bcb5e62702c360e
aliaslanoztoprak <aliaslan.oztoprak@gmail.com>
2019-10-20 15:25:54 +0300

    I've created a middleware for flash messages

--- app.js
+++ app.js
@@ -7,9 +7,41 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 
+const passport = require('passport');
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
+// Flash Middleware
+app.use(cookieParser('passporttutorial'));
+app.use(
+    session({
+    cookie: { maxAge: 60000 },
+    resave: true,
+    secret: "passporttutorial",
+    saveUninitialized: true,
+    })
+);
+app.use(flash());
+
+//Passport Initialize
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Global res.locals Middleware
+app.use((req, res, next) => {
+    // Our own flash
+    res.locals.successMessage = req.flash('successMessage');
+    res.locals.errorMessage = req.flash('errorMessage');
+
+    // Passport Flash
+    res.locals.passportFailure = req.flash('error');
+    res.locals.passportSuccess = req.flash('success');
+    
+    // Our Logged In User
+    res.locals.user = req.user;
+ 
+    next();
+});
 // MongoDB Connection
 mongoose.connect('mongodb://localhost/passportdb', {
     useNewUrlParser: true,
